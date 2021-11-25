57305b6a370c2e2d0655820f5857ee253bc99ce8
KoKaAeN <94991637+KoKaAeN@users.noreply.github.com>
2021-11-25 11:06:14 +0300

    Add files via upload

--- kokaenFiles/TagAdmins.lua
+++ kokaenFiles/TagAdmins.lua
@@ -2,7 +2,7 @@ local function TagAdmins(msg)
 local text = msg.content_.text_
 if ChatType == 'sp' or ChatType == 'gp'  then
 if Manager(msg) then
-if text == "admin" or text == "@admin" then
+if text == "المشرفين" or text == "تاك للمشرفين" then
 if DevAbs:get(kokaen.."Abs:TagAdmins:Time"..msg.chat_id_..':'..msg.sender_user_id_) then  
 Dev_Abs(msg.chat_id_, msg.id_, 1, '⌁︙انتظر دقيقه بعد ارسال الامر', 1, 'md')
 return false  
